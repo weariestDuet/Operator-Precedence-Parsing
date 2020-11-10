f0d949f1aee9be3049dc13eaa9e92f5cacc268f4
weariestDuet <1304662980@qq.com>
2020-11-10 18:02:33 +0800

    Update opg.cpp

--- opg.cpp
+++ opg.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-
+char input[1001];
 char store[1001];
 int top = 0;
 int matrix[6][6] = {
@@ -78,12 +78,14 @@ bool reduce()
 
 int main()
 {
-    std::string input;
-    std::cin >> input;
-    top = 0;
-    store[top++] = '#';
+
+    scanf("%s",input);
+    int len =strlen(input);
+    input[len]='#';
+    input[len+1]='\0';
+    store[top++]='#';
     int i=0;
-    for(int i=0;i<input.size();)
+    for(int i=0;i<len;)
     {
         int t = top-1;
         while (t >= 0 && !isTerminal(store[t]))
@@ -93,7 +95,7 @@ int main()
             break;
         }
         int u = getPr(store[t]);
-        int v = getPr(store[i]); 
+        int v = getPr(input[i]); 
         if (v == -1 || u == -1 || matrix[u][v] == -2){
             cout<<"E"<<endl;
             break;
@@ -101,8 +103,8 @@ int main()
         if(v == 5 && u == 5) break;
         if (matrix[u][v] <= 0) 
         {
-            cout<<"I"<<store[i]<<endl;
-            store[top++] = store[i];
+            cout<<"I"<<input[i]<<endl;
+            store[top++] = input[i];
             i++;
         }
         else
@@ -119,7 +121,7 @@ int main()
         }
     }
 
-    if(top > 2)
-        cout<<"RE"<<endl;
+    // if(top > 2)
+    //     cout<<"RE"<<endl;
     return 0;
 }
